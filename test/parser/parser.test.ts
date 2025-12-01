a2b7289948da4b0a5661932e61bbe9b35138b67b
iacobucci <iacobuccivalerio@gmail.com>
2025-12-01 14:26:48 +0100

    2025-12-01 14:26:48

--- test/parser/parser.test.ts
+++ test/parser/parser.test.ts
@@ -64,30 +64,4 @@ describe("parser", () => {
 
 	})
 
-	test("some more refined assembly", () => {
-		let ins = Array.from(InstructionRegistry.tagRegistry.keys());
-
-		// assuming every instruction is of type R, so it is of the form
-		// instr i<x>, i<y>, i<z>
-		// where x, y, z are integers from 0 to 31
-
-		const instruction = Rule.or("instruction", ...ins.map(x => Rule.literal(x)));
-		const spaces = Rule.zeroOrMore("spaces", Rule.literal(" "));
-		const comma = Rule.literal(",");
-		const index = Rule.or("index", ...range(0, 31).map(x => Rule.literal("" + x)));
-
-		const register = Rule.and("register", Rule.literal("i"), index);
-
-		const line = Rule.and("line", instruction, spaces, register, comma, spaces, register, comma, spaces, register);
-
-		const p = new Parser(line);
-
-		expect(p.parse("add i1, i2, i3").matched).toBe(true);
-		expect(p.parse("sub i1, i2, i3").matched).toBe(true);
-		expect(p.parse("add i10, i10, i10").matched).toBe(true);
-		expect(p.parse("add i100, i2, i3").matched).toBe(false);
-
-	})
-
-
 });
